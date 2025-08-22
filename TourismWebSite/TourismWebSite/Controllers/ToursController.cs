6d7be4dc76e653d638e669bfa6a22e2addf81855
Oussama <Hassouneoussama8@gmail.com>
2025-08-23 00:25:04 +1000

    .

---
+++ TourismWebSite/TourismWebSite/Controllers/ToursController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TourismWebSite.Controllers
+{
+    public class ToursController : Controller
+    {
+        // GET: Tours
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
