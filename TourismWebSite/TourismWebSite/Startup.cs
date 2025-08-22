efa32defd31f4a03efc999226ee47563e0bd30b5
Oussama <Hassouneoussama8@gmail.com>
2025-08-22 23:50:34 +1000

    ini

---
+++ TourismWebSite/TourismWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TourismWebSite.Startup))]
+namespace TourismWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
