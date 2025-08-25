05d24e27cdb83c63e778f843c8685e73476e65df
Oussama <Hassouneoussama8@gmail.com>
2025-08-25 15:05:02 +1000

    .

--- TourismWebSite/TourismWebSite/Controllers/ToursController.cs
+++ TourismWebSite/TourismWebSite/Controllers/ToursController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TourismWebSite.Models;
 
 namespace TourismWebSite.Controllers
 {
     public class ToursController : Controller
     {
         // GET: Tours
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var tours = db.Tours.ToList(); 
+
+            return View(tours);
+        }
+        public ActionResult Details(int id)
+        {
+            var tour = db.Tours.Find(id);
+            if (tour == null) return HttpNotFound();
+            return View(tour); 
         }
     }
 }
--- TourismWebSite/TourismWebSite/Models/Tours.cs
+++ TourismWebSite/TourismWebSite/Models/Tours.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace TourismWebSite.Models
+ namespace TourismWebSite.Models
 {
     public class Tours
     {
